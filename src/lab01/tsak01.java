4ee1f40224b134f845ead2ea38c88e6f0005fc27
VitalyDemachek <vilend77@mail.ru>
2018-01-15 19:41:36 +0600

    1 фрагмент 1 лаб.

---
+++ src/lab01/tsak01.java
@@ -0,0 +1,22 @@
+package lab01;
+
+public class tsak01 {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Привет!");
+        System.out.println("первая\nвторая");
+        System.out.println("+---+\n|   |\n|   |\n+---+");
+
+
+
+
+
+
+
+
+    }
+
+
+}
